907a604e7bc75103c6b8b4460effee34bda05cc5
kikutii <joaovictorkikuti@gmail.com>
2022-03-23 18:44:11 -0300

    terminado segundo requisito

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { About } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Verifica se a página contém as informações sobre a Pokédex.', () => {
+  beforeEach(() => renderWithRouter(<About />));
+
+  it('Verifica se a página contém um heading h2 com o texto About Pokédex', () => {
+    const title = screen.getByRole('heading', { level: 2, name: /about pokédex/i });
+    expect(title.textContent).toBe('About Pokédex');
+    expect(title).toBeInTheDocument();
+  });
+
+  it('Verifica se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
+    const describe = screen.getAllByText(/pokémons/i);
+    expect(describe.length).toBe(2);
+  });
+
+  it('Verifica se a página contém a imagem da Pokédex:', () => {
+    const image = screen.getByAltText('Pokédex');
+    expect(image.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    expect(image).toBeInTheDocument();
+  });
+});
