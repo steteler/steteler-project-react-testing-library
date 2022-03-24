5bc1f1936d714877ba22bf3d9e2114b6b10ebe7b
kikutii <joaovictorkikuti@gmail.com>
2022-03-24 16:48:48 -0300

    refatorado primeiro exercicio

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,35 +1,38 @@
 import userEvent from '@testing-library/user-event';
 import React from 'react';
+import { screen } from '@testing-library/react';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
 
 test('Verifica se o primeiro link contem o texto Home', () => {
-  const { getByRole, history } = renderWithRouter(<App />);
-  const linkHome = getByRole('link', { name: /home/i });
+  const { history } = renderWithRouter(<App />);
+  const linkHome = screen.getByRole('link', { name: /home/i });
   expect(linkHome).toBeInTheDocument();
   userEvent.click(linkHome);
   expect(history.location.pathname).toBe('/');
 });
 
 test('Verifica se o segundo link contem o texto About', () => {
-  const { getByRole, history } = renderWithRouter(<App />);
-  const linkAbout = getByRole('link', { name: /About/i });
+  const { history } = renderWithRouter(<App />);
+  const linkAbout = screen.getByRole('link', { name: /About/i });
   expect(linkAbout).toBeInTheDocument();
   userEvent.click(linkAbout);
   expect(history.location.pathname).toBe('/about');
 });
 
 test('Verifica se o terceiro link contem o texto Favorite Pokémons', () => {
-  const { getByRole, history } = renderWithRouter(<App />);
-  const linkFavorites = getByRole('link', { name: /Favorite Pokémons/i });
+  const { history } = renderWithRouter(<App />);
+  const linkFavorites = screen.getByRole('link', { name: /Favorite Pokémons/i });
   expect(linkFavorites).toBeInTheDocument();
   userEvent.click(linkFavorites);
   expect(history.location.pathname).toBe('/favorites');
 });
 
 test('Verifica se ao entrar em uma url desconhecida aparece a página Not Found', () => {
-  const { getByRole, history } = renderWithRouter(<App />);
+  const { history } = renderWithRouter(<App />);
   history.push('/url-desconhecida');
-  const notFound = getByRole('heading', { level: 2, name: /Page requested not found/i });
+  const notFound = screen.getByRole('heading', {
+    level: 2, name: /Page requested not found/i,
+  });
   expect(notFound).toBeInTheDocument();
 });
