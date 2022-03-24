fa3fbe152d630413e83c081faee838faa7ef29fd
kikutii <joaovictorkikuti@gmail.com>
2022-03-24 18:27:34 -0300

    terminado quarto exercício

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { NotFound } from '../components';
+import renderWithRouter from '../renderWithRouter';
+
+beforeEach(() => renderWithRouter(<NotFound />));
+
+test('Verifica se página contém um h2 com o texto Page requested not found 😭', () => {
+  const titleNotFound = screen.getByRole(
+    'heading',
+    { level: 2, name: /page requested not found/i },
+  );
+  expect(titleNotFound).toBeInTheDocument();
+});
+
+test('Verifica se página mostra a imagem do NotFound', () => {
+  const imageNotFound = screen.getByAltText(
+    /pikachu crying because the page requested was not found/i,
+  );
+  expect(imageNotFound).toBeInTheDocument();
+  expect(imageNotFound).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+});
