39602281fa8daa984ccc194fe850fc503ecfe853
kikutii <joaovictorkikuti@gmail.com>
2022-03-24 18:20:25 -0300

    refatorado primeiro exercicio, trocado nome das constantes

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -7,19 +7,19 @@ describe('Verifica se a página contém as informações sobre a Pokédex.', () 
   beforeEach(() => renderWithRouter(<About />));
 
   it('Verifica se a página contém um heading h2 com o texto About Pokédex', () => {
-    const title = screen.getByRole('heading', { level: 2, name: /about pokédex/i });
-    expect(title.textContent).toBe('About Pokédex');
-    expect(title).toBeInTheDocument();
+    const h2 = screen.getByRole('heading', { level: 2, name: /about pokédex/i });
+    expect(h2).toHaveTextContent('About Pokédex');
+    expect(h2).toBeInTheDocument();
   });
 
   it('Verifica se a página contém dois parágrafos com texto sobre a Pokédex.', () => {
-    const describe = screen.getAllByText(/pokémons/i);
-    expect(describe).toHaveLength(2);
+    const p = screen.getAllByText(/pokémons/i);
+    expect(p).toHaveLength(2);
   });
 
   it('Verifica se a página contém a imagem da Pokédex:', () => {
-    const image = screen.getByAltText('Pokédex');
-    expect(image.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
-    expect(image).toBeInTheDocument();
+    const img = screen.getByAltText(/pokédex/i);
+    expect(img.src).toBe('https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
+    expect(img).toBeInTheDocument();
   });
 });
