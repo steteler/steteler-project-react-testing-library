b18f4365a4af98f1295395b4d2341be203566467
kikutii <joaovictorkikuti@gmail.com>
2022-03-24 18:23:48 -0300

    refatorado segundo exercicio, troado nome das constantes

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -6,25 +6,25 @@ import renderWithRouter from '../renderWithRouter';
 
 test('Verifica se o primeiro link contem o texto Home', () => {
   const { history } = renderWithRouter(<App />);
-  const linkHome = screen.getByRole('link', { name: /home/i });
-  expect(linkHome).toBeInTheDocument();
-  userEvent.click(linkHome);
+  const home = screen.getByRole('link', { name: /home/i });
+  expect(home).toBeInTheDocument();
+  userEvent.click(home);
   expect(history.location.pathname).toBe('/');
 });
 
 test('Verifica se o segundo link contem o texto About', () => {
   const { history } = renderWithRouter(<App />);
-  const linkAbout = screen.getByRole('link', { name: /About/i });
-  expect(linkAbout).toBeInTheDocument();
-  userEvent.click(linkAbout);
+  const about = screen.getByRole('link', { name: /about/i });
+  expect(about).toBeInTheDocument();
+  userEvent.click(about);
   expect(history.location.pathname).toBe('/about');
 });
 
 test('Verifica se o terceiro link contem o texto Favorite Pokémons', () => {
   const { history } = renderWithRouter(<App />);
-  const linkFavorites = screen.getByRole('link', { name: /Favorite Pokémons/i });
-  expect(linkFavorites).toBeInTheDocument();
-  userEvent.click(linkFavorites);
+  const favorites = screen.getByRole('link', { name: /favorite pokémons/i });
+  expect(favorites).toBeInTheDocument();
+  userEvent.click(favorites);
   expect(history.location.pathname).toBe('/favorites');
 });
 
@@ -32,7 +32,7 @@ test('Verifica se ao entrar em uma url desconhecida aparece a página Not Found'
   const { history } = renderWithRouter(<App />);
   history.push('/url-desconhecida');
   const notFound = screen.getByRole('heading', {
-    level: 2, name: /Page requested not found/i,
+    level: 2, name: /page requested not found/i,
   });
   expect(notFound).toBeInTheDocument();
 });
